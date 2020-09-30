069397765a85b9aa9f07aa9da0b1949c9f6c5e2d
Brayan <brayan.057@live.cl>
2020-09-30 01:08:57 -0300

    agregando BD

--- proyecto_vicky/settings/production.py
+++ proyecto_vicky/settings/production.py
@@ -11,10 +11,10 @@ ALLOWED_HOSTS = ['proyectovicky.herokuapp.com']
 DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'proyecto_vicky',
-        'USER': 'postgres',
-        'PASSWORD': 'cindy057',
-        'HOST':'127.0.0.1',
+        'NAME': 'd7kun91l4tkktv',
+        'USER': 'gnozderoccpbft',
+        'PASSWORD': 'cf9df4ff7ba54bc5651cbcc055bb43156cd25d8201a1b870202722624ed7794c',
+        'HOST':'ec2-3-210-255-177.compute-1.amazonaws.com',
         'DATABASE_PORT':'5432',
    }
 }

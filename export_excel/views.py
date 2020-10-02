89c435790ceffca942415f833517686d59183b60
Brayan <brayan.057@live.cl>
2020-10-02 05:02:07 -0300

    arreglando error 35

--- export_excel/views.py
+++ export_excel/views.py
@@ -133,7 +133,7 @@ def index(request):
       #Filtro
       ws.auto_filter.ref='A1:' + max(ws.calculate_dimension()) + str(ws.max_row)
       wb.save('export_excel/static/files/proyecto_vicky.xlsx')
-      #wb.save('C:\\Users\\56975\\Documents\\proyecto_vicky.xlsx')
+      wb.save('C:\\Users\\56975\\Documents\\proyecto_vicky.xlsx')
       wb.save('C:\\Users\56975\Documents\proyecto_vicky.xlsx')
       wb.save(r'C:\Users\56975\Documents\proyecto_vicky.xlsx')
       #wb.save('C:/Users/56975/Documents/proyecto_vicky.xlsx')

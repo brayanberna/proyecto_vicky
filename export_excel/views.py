38cfc759b51ab41c22171172254408ce65b74b10
Brayan <brayan.057@live.cl>
2020-10-02 03:40:37 -0300

    arreglando error 29

--- export_excel/views.py
+++ export_excel/views.py
@@ -133,6 +133,7 @@ def index(request):
       #Filtro
       ws.auto_filter.ref='A1:' + max(ws.calculate_dimension()) + str(ws.max_row)
       wb.save('export_excel/static/files/proyecto_vicky.xlsx')
+      wb.save('export_excel/static/proyecto_vicky.xlsx')
       return redirect('index')
   return render(request, "index.html", {'form': form})
 

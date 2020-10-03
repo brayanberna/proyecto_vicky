9b999ef8469cc605d08461f70f49d86c1208e959
Brayan <brayan.057@live.cl>
2020-10-03 04:12:40 -0300

    arreglando error 39

--- proyecto_vicky/wsgi.py
+++ proyecto_vicky/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_vicky.settings.local')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_vicky.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_vicky.settings.local')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_vicky.settings.production')
 
 #application = get_wsgi_application()
 

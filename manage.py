2c95db75df4d8d7a88469d0f72869c7cb6731e9c
Brayan <brayan.057@live.cl>
2020-10-03 04:18:12 -0300

    arreglando error 40

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_vicky.settings.local')
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_vicky.settings.production')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_vicky.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_vicky.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

8f82b4246630504446bb505783625ed7156205ae
Brayan <brayan.057@live.cl>
2020-10-04 20:18:54 -0300

    arreglando error 62

--- proyecto_vicky/settings/base.py
+++ proyecto_vicky/settings/base.py
@@ -14,9 +14,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-import sys
-sys.getfilesystemencoding()
-
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.

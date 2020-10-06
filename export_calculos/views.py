2aa6bd32232767a59edb5a06a27aa47ac590ef81
Brayan <brayan.057@live.cl>
2020-10-06 05:08:09 -0300

    arreglando error 85

--- export_calculos/views.py
+++ export_calculos/views.py
@@ -180,7 +180,8 @@ def export(request):
     #wb2.save('C:/Users/56975/Documents/FORMATO CONTROL NIÑO SANO.xlsx')
     wb2.save('export_excel/static/files/resultado.xlsx')
     #response = HttpResponse(content_type='text/xlsx')
-    response = HttpResponse(content_type='application/vnd.ms-excel')
+    #response = HttpResponse(content_type='application/vnd.ms-excel')
+    response = HttpResponse(content_type='application/vnd.xls')
     response['Content-Disposition'] = 'attachment; filename="export_excel/static/files/resultado.xlsx"'
     #return redirect('export')
     return response

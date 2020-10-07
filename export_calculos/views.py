41e9a1ea413dab719303df56303a075f9a605707
Brayan <brayan.057@live.cl>
2020-10-07 01:48:19 -0300

    arreglando error 123

--- export_calculos/views.py
+++ export_calculos/views.py
@@ -181,7 +181,7 @@ def export(request):
 
     #wb2.save('export_excel/static/files/FORMATO CONTROL NIÑO SANO.xlsx'.encode('utf-8'))
     #wb2.save(str(export_excel/static/files/FORMATO CONTROL NIÑO SANO.xlsx).encode('utf-8'))
-    nombre = "export_excel/static/files/FORMATO CONTROL NIÑO SANO.xlsx".encode('utf-8')
+    nombre = "export_excel/static/files/FORMATO CONTROL NIÑO SANO".encode('utf-8')
     #wb2.save(str('export_excel/static/files/FORMATO CONTROL NIÑO SANO').encode('utf-8') + '.xlsx')
     wb2.save('nombre.xlsx')
     #$('#imagePreview').html("<img src='"+e.target.result+"' width='"+180+"' height='"+170+"' class='"+'img-fluid'+"' />");

a64fd1ae507c4128a4b65076a1e2924faa547fc5
Brayan <brayan.057@live.cl>
2020-10-07 04:02:21 -0300

    arreglando error 131

--- export_calculos/views.py
+++ export_calculos/views.py
@@ -179,17 +179,18 @@ def export(request):
     ws2['X34'] = dieciocho_veintitres_meses_m
     ws2['Y34'] = dieciocho_veintitres_meses_f
 
+    wb2.save('export_excel/static/files/FORMATO CONTROL NIÑO SANO.xlsx')
     #wb2.save('export_excel/static/files/FORMATO CONTROL NIÑO SANO.xlsx').encode('utf-8')
     #wb2.save('export_excel/static/files/FORMATO CONTROL NIÑO SANO.xlsx'.encode('utf-8'))
     #wb2.save('export_excel/static/files/FORMATO CONTROL NIÑO SANO.xlsx'.encode('utf-8'))
     #wb2.save(str('export_excel/static/files/FORMATO CONTROL NIÑO SANO.xlsx'.encode('utf-8') + '.xlsx'))
     #wb2.save('export_excel/static/files/FORMATO CONTROL NI'+'Ñ'.encode('utf-8')+'O SANO.xlsx')
     #wb2.save(str(export_excel/static/files/FORMATO CONTROL NIÑO SANO.xlsx).encode('utf-8'))
-    nombre = "export_excel/static/files/FORMATO CONTROL NIÑO SANO.xlsx".encode('utf-8')
+    #nombre = "export_excel/static/files/FORMATO CONTROL NIÑO SANO.xlsx".encode('utf-8')
     #wb2.save(str('export_excel/static/files/FORMATO CONTROL NIÑO SANO').encode('utf-8') + '.xlsx')
     #wb2.save('%s.xlsx'%nombre)
-    wb2.save('%s'%nombre)
-    wb2.save(nombre)
+    #wb2.save('%s'%nombre)
+    #wb2.save(nombre)
     #wb2.save(nombre + '.xlsx')
     #$('#imagePreview').html("<img src='"+e.target.result+"' width='"+180+"' height='"+170+"' class='"+'img-fluid'+"' />");
     #wb2.save(str('export_excel/static/files/FORMATO CONTROL NIÑO SANO').encode('utf-8').xlsx)
